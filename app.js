58a512ffa7017f493683d5c212d9985b0d3185fc
Damiana-Arangio <arangio.damiana@gmail.com>
2025-10-16 00:21:02 +0200

    Gestito avvio server e configurata rotta "/"

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-/***************
-    BLOG
-***************/
-console.log("prova1");
+/************************************BLOG SERVER*************************************/
+
+const express = require('express')  // Import del framework Express
+const chalk = require("chalk");     // Import del pacchetto chalk
+
+/***************************
+    CONFIGURAZIONE EXPRESS
+****************************/
+const app = express()            // Inizializzazione dell'app Express
+const port = 3000;               // Definizione porta su cui il server deve rimanere in ascolto
+
+
+/************
+    ROTTE
+************/
+
+/* Rotta Principale */
+app.get('/', (req, res) => {
+    res.send('Server del mio blog') // Testo da inviare al client quando arriva una richiesta HTTP di tipo GET all'indirizzo "/"
+})
+    
+
+/*********************
+    AVVIO SERVER
+*********************/
+/* Il server viene messo in ascolto sulla porta 3000 */
+app.listen(port, () => {
+    console.log(chalk.red("Server in ascolto sulla porta " + port));
+})
